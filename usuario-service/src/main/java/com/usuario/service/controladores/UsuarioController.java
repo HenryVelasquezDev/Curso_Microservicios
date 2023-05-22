9f551ba1c9d220220026ef64447f109b2e73a907
henry.velasquez <henryalex25@gmail.com>
2023-05-22 12:52:27 -0500

    Nueva comunicacion desde UsuarioService con CarroService y MotoService por medio de RestTemplate

--- usuario-service/src/main/java/com/usuario/service/controladores/UsuarioController.java
+++ usuario-service/src/main/java/com/usuario/service/controladores/UsuarioController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usuario.service.entidades.Usuario;
+import com.usuario.service.modelos.Carro;
+import com.usuario.service.modelos.Moto;
 import com.usuario.service.servicio.UsuarioService;
 
 @RestController
@@ -45,5 +47,27 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> guardarUsuario(@RequestBody Usuario usuario){
 		Usuario nuevoUsuario = usuarioService.save(usuario);
 		return ResponseEntity.ok( nuevoUsuario );
+	}
+	
+	@GetMapping("/carros/{usuarioId}")
+	public ResponseEntity<List<Carro>> listarCarros(@PathVariable("usuarioId") int usuarioId){
+		Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		List<Carro> carros = usuarioService.getCarros(usuarioId);
+		return ResponseEntity.ok( carros );
+	}
+	
+	@GetMapping("/motos/{usuarioId}")
+	public ResponseEntity<List<Moto>> listarMotos(@PathVariable("usuarioId") int usuarioId){
+		Usuario usuario = usuarioService.getUsuarioById(usuarioId);
+		if (usuario == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		List<Moto> motos = usuarioService.getMotos(usuarioId);
+		return ResponseEntity.ok( motos );
 	}
 }
