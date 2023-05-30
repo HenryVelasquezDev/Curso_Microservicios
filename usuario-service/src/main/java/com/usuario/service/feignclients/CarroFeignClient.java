19b082ed3e093266bfaec8ff90f12ccae9944e79
henry.velasquez <henryalex25@gmail.com>
2023-05-30 17:54:19 -0500

    se actualiza feignCliente y se cambia configuracion de gateway-service yaml

--- usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.service.modelos.Carro;
 
-@FeignClient( name = "carro-service",url = "http://localhost:8002")
+@FeignClient( name = "carro-service"/*,url = "http://localhost:8002"*/)//por uso de gateway ya no se puede dejar lña url estatica
 @RequestMapping("/carro")
 public interface CarroFeignClient {
 
